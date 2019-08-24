7b692f30e592113d0b6274d585287c83ca4ab091
Keirron Stach <stachkeirron@gmail.com>
2019-08-24 15:14:33 +0930

    First commit, Bootstrapped templeate

---
+++ BrackeysGameJam/Assets/Scripts/MasterManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MasterManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static MasterManager _instance;
+
+    #endregion
+
+    #region Scenes
+
+    [Header("Scenes")]
+    [SerializeField]
+    private string mainMenuScene;
+
+    [SerializeField]
+    private string gameScene;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+            SceneManager.LoadScene(mainMenuScene);
+        }
+    }
+
+    public static void MainMenu()
+    {
+        SceneManager.LoadScene(_instance.mainMenuScene);
+    }
+
+    public static void Game()
+    {
+        SceneManager.LoadScene(_instance.gameScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ BrackeysGameJam/Assets/Scripts/Screens/MainMenuScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuScreen : MonoBehaviour
+{
+    [SerializeField]
+    private Button startGameButton;
+
+    private void OnEnable()
+    {
+        startGameButton.onClick.AddListener(OnStartGameClick);
+    }
+
+    private void OnDisable()
+    {
+        startGameButton.onClick.RemoveListener(OnStartGameClick);
+    }
+
+    private void OnStartGameClick()
+    {
+        MasterManager.Game();
+    }
+}
